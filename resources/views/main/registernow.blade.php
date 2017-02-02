f8aef20178af229882e914158badcba46114327d
adityacodes <adityapadhi1996@gmail.com>
2017-02-03 01:57:36 +0530

    fixed register ifram src

--- resources/views/main/registernow.blade.php
+++ resources/views/main/registernow.blade.php
@@ -10,7 +10,7 @@
 		<div class="row m-t-120">
         	<div class="col-md-offset-2 col-md-10">
         		<h1 class="green-color"><strong>REGISTER</strong></h1>
-        		<iframe style="overflow-y: none !important; " frameborder="0" width="1055" height="1055" src="http://localhost:8000/register"></iframe>
+        		<iframe style="overflow-y: none !important; " frameborder="0" width="1055" height="1055" src="http://secureweb.adityacodes.xyz/register"></iframe>
 
         		
             </div>
